389e07ced8b0b0d3e28c268ff640c1c063a15824
Johan Wilfer <johan@jttech.se>
2024-07-07 14:44:37 +0200

    use symfony ecs sets from PHP CS

--- ecs.php
+++ ecs.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\FixerFactory;
+use PhpCsFixer\RuleSet\RuleSet;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return ECSConfig::configure()
+$config = ECSConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/examples',
@@ -21,3 +22,24 @@ return ECSConfig::configure()
         namespaces: true,
     )
 ;
+
+// source: https://hugo.alliau.me/blog/posts/2023-07-19-how-to-use-php-cs-fixer-ruleset-with-easy-coding-standard
+// Configure Symfony and Symfony Risky SetList from PHP-CS-Fixer, since they are not shipped anymore with Easy Coding Standard.
+$fixerFactory = new FixerFactory();
+$fixerFactory->registerBuiltInFixers();
+$ruleSet = new RuleSet([
+    '@Symfony' => true,
+    // You can also enable the risky ruleset if you want.
+    '@Symfony:risky' => true,
+]);
+$fixerFactory->useRuleSet($ruleSet);
+
+foreach ($fixerFactory->getFixers() as $fixer) {
+    if (null !== $fixerConfiguration = $ruleSet->getRuleConfiguration($fixer->getName())) {
+        $config->withConfiguredRule($fixer::class, $fixerConfiguration);
+    } else {
+        $config->withRules([$fixer::class]);
+    }
+}
+
+return $config;
